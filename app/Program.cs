e27c7623b32fb5539c146d03a383d550dad38b6a
john <jrborges52@gmail.com>
2022-09-10 11:54:02 -0700

    exercise one

---
+++ app/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Training
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.Write("Enter a number: ");
+      double numberOne = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Enter a second number: ");
+      double numberTwo = Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine("---");
+      Console.WriteLine(numberOne + numberTwo);
+
+
+      Console.WriteLine("---");
+    }
+  }
+}
