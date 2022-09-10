f6292230dd35155c2386b05e8e10669768298509
john <jrborges52@gmail.com>
2022-09-10 12:40:00 -0700

    function practice

--- app/Program.cs
+++ app/Program.cs
@@ -5,16 +5,22 @@
 
     static void Main(string[] args)
     {
-      Console.Write("Enter a number: ");
-      double numberOne = Convert.ToDouble(Console.ReadLine());
-      Console.Write("Enter a second number: ");
-      double numberTwo = Convert.ToDouble(Console.ReadLine());
-
-      Console.WriteLine("---");
-      Console.WriteLine(numberOne + numberTwo);
 
 
       Console.WriteLine("---");
+      Console.WriteLine("Whats your name? ");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("How old are you? ");
+      int age = Convert.ToInt32(Console.ReadLine());
+
+      Bibico(name, age);
+
+      Console.WriteLine("---");
+    }
+    static void Bibico(string name, int age)
+    {
+      Console.WriteLine("Hello " + name + " you are " + age + "years old");
     }
   }
 }
